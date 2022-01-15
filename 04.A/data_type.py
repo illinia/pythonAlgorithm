d626b19fe4364bee01c97fe688aaa983a56618e7
illinia <seolin1218@gmail.com>
2022-01-15 23:24:03 +0900

    파이썬 알고리즘 공부 자료형, 조건문

---
+++ 04.A/data_type.py
@@ -0,0 +1,182 @@
+# a = 1000
+# print(a)
+#
+# a = -7
+# print(a)
+#
+# a = 0
+# print(a)
+#
+# a = 157.93
+# print(a)
+#
+# a = -1837.2
+# print(a)
+#
+# a = 5.
+# print(a)
+#
+# a = -.7
+# print(a)
+#
+# a = 1e9
+# print(a)
+#
+# a = 75.25e1
+# print(a)
+#
+# a = 3954e-3
+# print(a)
+#
+# a = 0.3 + 0.6
+# print(a)
+#
+# if a == 0.9:
+#     print(True)
+# else:
+#     print(False)
+#
+# a = 0.3 + 0.6
+# print(round(a, 4))
+#
+# if round(a, 4) == 0.9:
+#     print(True)
+# else:
+#     print(False)
+#
+# a = 7
+# b = 3
+# print(a / b)
+# print(a % b)
+# print(a // b)
+#
+# a = 5
+# b = 3
+# print(a ** b)
+#
+# a = [1,2,3,4,5,6,7,8,9]
+# print(a)
+#
+# print(a[4])
+#
+# a = list()
+# print(a)
+#
+# a = []
+# print(a)
+#
+# n = 10
+# a = [0] * n
+# print(a)
+#
+# a = [1,2,3,4,5,6,7,8,9]
+# print(a[-1])
+#
+# print(a[-3])
+#
+# a[3] = 7
+# print(a)
+#
+# a = [1,2,3,4,5,6,7,8,9]
+# print(a[1:4])
+#
+# array = [i for i in range(20) if i % 2 == 1]
+# print(array)
+#
+# array = [i * i for i in range(1, 10)]
+# print(array)
+#
+# n = 3
+# m = 4
+# array = [[0] * m for _ in range(n)]
+# print(array)
+#
+# a = [1,4,3]
+# print('기본 리스트: ', a)
+#
+# a.append(2)
+# print('삽입: ', a)
+#
+# a.sort()
+# print('오름차순 정렬: ', a)
+#
+# a.sort(reverse=True)
+# print('내림차순 정렬: ', a)
+#
+# a.reverse()
+# print('원소 뒤집기: ', a)
+#
+# a.insert(2, 3)
+# print('값이 3인 데이터 개수: ', a.count(3))
+#
+# a.remove(1)
+# print('값이 1인 데이터 삭제: ', a)
+#
+# a = [1,2,3,4,5,5,5]
+# remove_set = {3, 5}
+#
+# result = [i for i in a if i not in remove_set]
+# print(result)
+#
+# data = 'Hello World'
+# print(data)
+#
+# data = "Don't you know \"Python\"?"
+# print(data)
+#
+# a = 'Hello'
+# b = 'World'
+# print(a + ' ' + b)
+#
+# a = 'String'
+# print(a * 3)
+#
+# a = 'ABCDEF'
+# print(a[2:4])
+#
+# a = (1,2,3,4)
+# print(a)
+# a[2] = 7
+
+# data = dict()
+# data['사과'] = 'Apple'
+# data['바나나'] = 'Banana'
+# data['코코넛'] = 'Coconut'
+#
+# print(data)
+#
+# if '사과' in data:
+#     print("'사과'를 키로 가지는 데이터가 존재합니다.")
+#
+# key_list = data.keys()
+# value_list = data.values()
+# print(key_list)
+# print(value_list)
+#
+# for key in key_list:
+#     print(data[key])
+#
+# data = set([1,1,2,3,4,4,5])
+# print(data)
+#
+# data = {1,1,2,3,4,4,5}
+# print(data)
+#
+# a = set([1,2,3,4,5])
+# b = set([3,4,5,6,7])
+#
+# print(a | b)
+# print(a & b)
+# print(a - b)
+#
+# data = set([1,2,3])
+# print(data)
+#
+# data.add(4)
+# print(data)
+#
+# data.update([5, 6])
+# print(data)
+#
+# data.remove(3)
+# print(data)
